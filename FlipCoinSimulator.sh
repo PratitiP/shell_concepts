07bc20e6c3ccac7cffe47701e74cd074a47bd812
Pratiti <pratitipshah@gmail.com>
2020-01-10 17:27:37 +0530

    Added : Percentage of singlet combination

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,8 +1,24 @@
 #!/bin/bash -x
 
-r=$((RANDOM%2))
-if (( r==1))
-then echo Head
-else
-	echo Tail
-fi
+function getFlipResult(){
+	r=$((RANDOM%2))
+	if (( r==1))
+	then 
+		resultDict[$r]=$(( ${resultDict[$r]} + 1 ))
+	else
+		resultDict[$r]=$(( ${resultDict[$r]} + 1 ))
+	fi
+}
+
+declare -A resultDict
+resultDict=([0]=0 [1]=0)
+read -p "How many times you want to flip a single coin : " n
+for (( i=0 ; i<n ; i++))
+do
+	getFlipResult
+done
+
+echo "Percentage of H = $(bc <<< "scale=2; (${resultDict[1]}*100)/$n")%"
+echo "Percentage of T = $(bc <<< "scale=2; (${resultDict[0]}*100)/$n")%"
+
+
