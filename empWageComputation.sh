5cb2e045e5ce87458d0c03cfc9b4922c84c97cca
Pratiti <pratitipshah@gmail.com>
2020-01-10 11:44:04 +0530

    Added : Monthly wage calculations

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,32 +1,38 @@
-##!/bin/bash -x
+#!/bin/bash -x
 
 #constants for wage calculation
 WAGE_PER_HR=20
 FULL_DAY=1
 PART_TIME=2
+MONTH_WORKING_DAYS=20
+
+#variables for wage calculations
+monthlyWage=0
+dailyWage=0
 
 echo Welcome to Employee Wage Computation Program
 
 #check attendance & add empHrs accordingly
-randAttend=$((RANDOM%3));
-#modified for switch case
-case $randAttend in
-	$FULL_DAY)
-		echo "Employee is Present"
-		empHrs=8
-		;;
-	$PART_TIME)
-		echo "Employee is Part Time"
-		empHrs=4
-		;;
-	*)
-		echo "Employee is absent"
-		empHrs=0
-		;;
-esac
+#calculate for a month
+for (( day=1 ; day<=$MONTH_WORKING_DAYS ; day++ ))
+do
+	randAttend=$((RANDOM%3));
+	case $randAttend in
+		$FULL_DAY)
+			empHrs=8
+			;;
+		$PART_TIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	#calculate monthly wage
+	dailyWage=$(( WAGE_PER_HR * empHrs))
+	monthlyWage=$(( monthlyWage + dailyWage))
+done
 
-#calculate daliy wage
-dailyWage=$(( WAGE_PER_HR * empHrs))
-echo "Daily wage = $dailyWage"
+echo "Monthly Salary = $monthlyWage"
 
 
