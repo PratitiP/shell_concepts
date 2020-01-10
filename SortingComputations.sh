a81d5f93dc86b93eb2399fc12737706d82288a5d
Pratiti <pratitipshah@gmail.com>
2020-01-10 15:04:47 +0530

    Added : values to an array from dictionary

--- SortingComputations.sh
+++ SortingComputations.sh
@@ -2,13 +2,24 @@
 
 read -p "Enter 3 numbers : " a b c
 echo $a $b $c
+#Store computations into dict
 declare -A comput
 comput['a+b*c']=$(( a + b * c ))
 comput['a*b+c']=$(( a * b + c ))
 comput['c+a/b']=$(( c + a / b ))
 comput['a%b+c']=$(( a%b+c))
 
+echo "Computations stored in dictionary"
+#simulteneously store values into array
+c=0
 for i in ${!comput[@]}
 do
-	echo "$i -> ${comput[$i]}"
+	echo "[$i] : ${comput[$i]}"
+	computArr[$c]=${comput[$i]}
+	(( c++ ))
 done
+
+#show stored values in array
+echo "Array of computations = [ ${computArr[@]} ]"
+
+
