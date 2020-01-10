c653e1de1d7988569dd78cf59c5d78388f7044ed
Pratiti <pratitipshah@gmail.com>
2020-01-10 11:16:10 +0530

    Refactor : for part time UC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,29 @@
-#!/bin/bash -x
+##!/bin/bash -x
 
 #constants for wage calculation
 WAGE_PER_HR=20
-FULL_DAY_HRS=8
+FULL_DAY=1
+PART_TIME=2
 
 echo Welcome to Employee Wage Computation Program
 
-#check attendance using Random no
-randAttend=$((RANDOM%2));
-if [[ $randAttend == 1 ]]
+#check attendance & add empHrs accordingly
+randAttend=$((RANDOM%3));
+if [[ $randAttend == $FULL_DAY ]]
 then
 	echo "Employee is Present"
+	empHrs=8
+elif [[ $randAttend == $PART_TIME ]]
+then
+	echo "Employee is Part Time"
+	empHrs=4
 else
-	echo "Employee is absent";
+	echo "Employee is absent"
+	empHrs=0
 fi
 
 #calculate daliy wage
-dailyWage=$(( WAGE_PER_HR * FULL_DAY_HRS * randAttend))
+dailyWage=$(( WAGE_PER_HR * empHrs))
 echo "Daily wage = $dailyWage"
 
 
