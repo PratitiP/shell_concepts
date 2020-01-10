09f8c8ae3856731f158d85ccc65c15cdcb36305e
tekina.makin <tekina.makin@gmail.com>
2020-01-10 10:31:39 +0530

    Added random check for employee attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,10 @@
+#!/bin/bash -x
 echo Welcome to Employee Wage Computation Program
+
+randAttendance=$((RANDOM%2));
+if [[ randAttendance == 1 ]]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is absent";
+fi
