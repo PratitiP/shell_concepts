d47cf08259feb4136b1d443380581682483d86f3
Pratiti <pratitipshah@gmail.com>
2020-01-10 15:55:46 +0530

    Added : sorted array Descending and Ascending

--- SortingComputations.sh
+++ SortingComputations.sh
@@ -1,5 +1,40 @@
 #!/bin/bash -x
 
+function sortAscending(){
+	size=${#computArr[@]}
+	for (( i=0 ; i<size ; i++ ))
+	do
+		for(( j=$(($i+1)) ; j<size ; j++ ))
+		do
+			if [[ ${computArr[$i]} -gt ${computArr[$j]} ]]
+			then
+				temp=${computArr[$i]}
+				computArr[i]=${computArr[$j]}
+				computArr[j]=$temp
+			fi
+		done
+	done
+	echo "Array sorted in Ascending order : ${computArr[@]}"
+}
+
+function sortDescending(){
+	size=${#computArr[@]}
+	for (( i=0 ; i<size ; i++ ))
+	do
+		for(( j=$(($i+1)) ; j<size ; j++ ))
+		do
+			if [[ ${computArr[$i]} -lt ${computArr[$j]} ]]
+			then
+				temp=${computArr[$i]}
+				computArr[i]=${computArr[$j]}
+				computArr[j]=$temp
+			fi
+		done
+	done
+	echo "Array sorted in descending order : ${computArr[@]}"
+}
+
+
 read -p "Enter 3 numbers : " a b c
 echo $a $b $c
 #Store computations into dict
@@ -21,5 +56,6 @@ done
 
 #show stored values in array
 echo "Array of computations = [ ${computArr[@]} ]"
-
+sortDescending
+sortAscending
 
