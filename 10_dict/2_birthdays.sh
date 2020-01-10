656452b3569074af056d581d281c503ffb96270c
tekina.makin <tekina.makin@gmail.com>
2020-01-10 09:52:36 +0530

    2_birthday.sh modified for keys as month names

--- 10_dict/2_birthdays.sh
+++ 10_dict/2_birthdays.sh
@@ -1,14 +1,25 @@
+#!/bin/bash -x
 function get_birth_months(){
 	r=$(($(($RANDOM%12))+1))
-	dict[$r]=$((${dict[$r]}+1))
+	case $r in 
+		1)dict['Jan']=$((${dict['Jan']}+1));;
+		2)dict['Feb']=$((${dict['Feb']}+1));;
+		3)dict['March']=$((${dict['March']}+1));;
+		4)dict['April']=$((${dict['April']}+1));;
+		5)dict['May']=$((${dict['May']}+1));;
+		6)dict['June']=$((${dict['June']}+1));;
+		7)dict['July']=$((${dict['July']}+1));;
+		8)dict['Aug']=$((${dict['Aug']}+1));;
+		9)dict['Sept']=$((${dict['Sept']}+1));;
+		10)dict['Oct']=$((${dict['Oct']}+1));;
+		11)dict['Nov']=$((${dict['Nov']}+1));;
+		12)dict['Dec']=$((${dict['Dec']}+1));;
+	esac
 }
 
 declare -A dict
-for (( i=1 ; i<=12 ; i++))
-do
-	dict[$i]=0
-done
-
+dict=(['Jan']=0 ['Feb']=0 ['March']=0 ['April']=0 ['May']=0 ['June']=0 ['July']=0 ['Aug']=0 ['Sept']=0 ['Oct']=0 ['Nov']=0 ['Dec']=0)
+echo ${!dict[@]}
 c=1
 while (( c <= 50))
 do
@@ -21,4 +32,3 @@ do
 	echo "Individuals having birthday in month $i = ${dict[$i]}"
 done
 
-echo "${!dict[@]}"
