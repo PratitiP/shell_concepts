bccc716b2150fea62e26522c63c4401e6e1a7172
Pratiti <pratitipshah@gmail.com>
2020-01-10 14:56:11 +0530

    modified : stored results into dict UC6

--- SortingComputations.sh
+++ SortingComputations.sh
@@ -2,7 +2,13 @@
 
 read -p "Enter 3 numbers : " a b c
 echo $a $b $c
-echo "a+b*c = $(( a + b * c ))"
-echo "a*b+c = $(( a * b + c ))"
-echo "c+a/b = $(( c + a / b ))"
-echo "a%b+c = $(( a%b+c))"
+declare -A comput
+comput['a+b*c']=$(( a + b * c ))
+comput['a*b+c']=$(( a * b + c ))
+comput['c+a/b']=$(( c + a / b ))
+comput['a%b+c']=$(( a%b+c))
+
+for i in ${!comput[@]}
+do
+	echo "$i -> ${comput[$i]}"
+done
