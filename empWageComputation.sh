f0eaad8450bb780f8d63646cdedeaa9bb837071b
tekina.makin <tekina.makin@gmail.com>
2020-01-10 10:57:48 +0530

    Added : daily wage calculations

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
+
+#constants for wage calculation
+WAGE_PER_HR=20
+FULL_DAY_HRS=8
+
 echo Welcome to Employee Wage Computation Program
 
-randAttendance=$((RANDOM%2));
-if [[ randAttendance == 1 ]]
+#check attendance using Random no
+randAttend=$((RANDOM%2));
+if [[ $randAttend == 1 ]]
 then
 	echo "Employee is Present"
 else
 	echo "Employee is absent";
 fi
+
+#calculate daliy wage
+dailyWage=$(( WAGE_PER_HR * FULL_DAY_HRS * randAttend))
+echo "Daily wage = $dailyWage"
+
+
