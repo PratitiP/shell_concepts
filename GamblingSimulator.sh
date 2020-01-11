9a94e0140564bf06ff72fcb0521a63b1b7ffa96f
Pratiti <pratitipshah@gmail.com>
2020-01-11 12:06:29 +0530

    Modified : for next month continue if won

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -11,41 +11,57 @@ stakeWin=0
 stakeLost=0
 dayWin=0
 dayLost=0
-for(( i=1 ; i<=20 ; i++ ))
+f=1
+while((f==1))
 do
-	startStake=$EVERY_DAY_START_STAKE
-	stake=$startStake
-	noOfBets=0
-	resignWin=$((startStake+startStake/2))
-	resignLost=$((startStake/2))
-	while(( (stake<resignWin) && (stake>resignLost) ))
+	for(( i=1 ; i<=20 ; i++ ))
 	do
-		((noOfBets++))
-		randBet=$((RANDOM%2))
-		if ((randBet==1))
+		startStake=$EVERY_DAY_START_STAKE
+		stake=$startStake
+		noOfBets=0
+		resignWin=$((startStake+startStake/2))
+		resignLost=$((startStake/2))
+		while(( (stake<resignWin) && (stake>resignLost) ))
+		do
+			((noOfBets++))
+			randBet=$((RANDOM%2))
+			if ((randBet==1))
+			then
+				stake=$((stake+BET))
+			else
+				stake=$((stake-BET))
+			fi
+		done
+		if((stake==resignWin))
 		then
-			stake=$((stake+BET))
+			stakeWin=$((stakeWin+stake))
+			echo -e "You Won for the day $i = lucky day"
+			#echo "You played $noOfBets Bets to have stake = \$$stake"
+			echo -e "Total amount Won = \$$stake\n"
+			((dayWin++))
 		else
-			stake=$((stake-BET))
+			stakeLost=$((stakeLost+stake))
+			echo -e "You Lost for the day $i = Unlucky day"
+			#echo "You played $noOfBets Bets to have stake = \$$stake"
+			echo -e "Total amount Lost = \$$stake\n"
+			((dayLost++))
 		fi
 	done
-	if((stake==resignWin))
+
+	echo -e "------------------------\nAfter a month\n-----------------------"
+	echo -e "No of days you won the game =  $dayWin days\nTotal amount Won = $stakeWin"
+	echo -e "No of days you lost the game = $dayLost days\nTotal amount Lost = $stakeLost"
+
+	result=$((dayWin*50-dayLost*50))
+	if((result>0))
 	then
-		stakeWin=$((stakeWin+stake))
-		echo -e "You Won for the day $i = lucky day"
-		#echo "You played $noOfBets Bets to have stake = \$$stake"
-		echo -e "Total amount Won = \$$stake\n"
-		((dayWin++))
+		echo "You Won for the month. Would you like to continue (y/n) : "
+		read continue
+		if [[ $continue == 'n' ]]
+		then f=0
+		fi
 	else
-		stakeLost=$((stakeLost+stake))
-		echo -e "You Lost for the day $i = Unlucky day"
-		#echo "You played $noOfBets Bets to have stake = \$$stake"
-		echo -e "Total amount Lost = \$$stake\n"
-		((dayLost++))
+		echo "You lost this month. You have to stop"
+		f=0
 	fi
 done
-
-echo -e "------------------------\nAfter a month\n-----------------------"
-echo -e "No of days you won the game =  $dayWin days\nTotal amount Won = $stakeWin"
-echo -e "No of days you lost the game = $dayLost days\nTotal amount Lost = $stakeLost"
-
