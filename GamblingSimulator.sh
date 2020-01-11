5f61a35d8cf6d3b33346e32909827d164db84685
Pratiti <pratitipshah@gmail.com>
2020-01-11 09:44:07 +0530

    Added : loose or win condition for each bet

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,6 +1,22 @@
 #!/bin/bash -x
 
+#constants for gambling simulation
 EVERY_DAY_START_STAKE=100
 BET=1
 
-echo "Welcome to Gambling Simulator"
+stake=100
+
+echo -e "Welcome to Gambling Simulator\n----------------------------"
+echo -e "Every day Gambler can start with \$100\nEvery game you can bet \$1\n----------------------------"
+
+randBet=$((RANDOM%2))
+if ((randBet==1))
+then
+	echo "You won \$1"
+	(( stake++ ))
+else
+	echo "You lost \$1"
+	((stake--))
+fi
+
+echo "stake = \$$stake"
