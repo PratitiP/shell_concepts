2ac3a857591a4193aa1837fd6bb826940d941ef4
Pratiti <pratitipshah@gmail.com>
2020-01-11 10:07:46 +0530

    Added : UC3 - resign for the day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -5,18 +5,28 @@ EVERY_DAY_START_STAKE=100
 BET=1
 
 stake=100
-
+startStake=100
+noOfBets=0
 echo -e "Welcome to Gambling Simulator\n----------------------------"
 echo -e "Every day Gambler can start with \$100\nEvery game you can bet \$1\n----------------------------"
 
-randBet=$((RANDOM%2))
-if ((randBet==1))
-then
-	echo "You won \$1"
-	(( stake++ ))
-else
-	echo "You lost \$1"
-	((stake--))
-fi
+resignWin=$((startStake+startStake/2))
+resignLost=$((startStake/2))
+while(( (stake<resignWin) && (stake>resignLost) ))
+do
+	((noOfBets++))
+	randBet=$((RANDOM%2))
+	if ((randBet==1))
+	then
+		((stake++))
+	else
+		((stake--))
+	fi
+done
 
-echo "stake = \$$stake"
+if((stake==resignWin))
+then
+	echo -e "You Won for the day\nYou played $noOfBets Bets to have stake = \$$stake"
+else
+	echo -e "You Lost for the day\nYou played $noOfBets Bets to have stake = \$$stake"
+fi
