5e9eeebb202762fe6f8e4432d1c90ada4f586f06
Pratiti <pratitipshah@gmail.com>
2020-01-13 15:05:32 +0530

    Added : UC2 - assigned a symbol to user

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -9,4 +9,16 @@ function print(){
 	echo -e "$var |$var |$var"
 }
 
+user=$((RANDOM%2))
+if((user==0))
+then
+	symbolU='0'
+	symbolC='X'
+else
+	symbolU='X'
+	symbolC='0'
+fi
+
 print
+echo "Your symbol is : $symbolU"
+
