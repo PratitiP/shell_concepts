89c6a8d92d2c13a37d3aeb76cc96748496c21d8d
Pratiti <pratitipshah@gmail.com>
2020-01-13 16:01:58 +0530

    Added : UC4- values to be entered by user to choose valid cell

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,11 +2,22 @@
 
 echo "Welcome to Tic-Tac-Toe"
 
+declare -A board
+board[0,0]=00
+board[0,1]=01
+board[0,2]=02
+board[1,0]=10
+board[1,1]=11
+board[1,2]=12
+board[2,0]=20
+board[2,1]=21
+board[2,2]=22
+
 function print(){
 	var=" "
-	echo -e "$var |$var |$var\n-- -- --"
-	echo -e "$var |$var |$var\n-- -- --"
-	echo -e "$var |$var |$var"
+	echo -e "${board[0,0]} |${board[0,1]} |${board[0,2]}\n--- --- ---"
+	echo -e "${board[1,0]} |${board[1,1]} |${board[1,2]}\n--- --- ---"
+	echo -e "${board[2,0]} |${board[2,1]} |${board[2,2]}"
 }
 
 user=$((RANDOM%2))
@@ -32,3 +43,4 @@ else
 	player=1
 	echo "Computer will play first"
 fi
+
