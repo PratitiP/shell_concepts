0a3888d0c80e452e87e556c20742ea5b4112732b
Pratiti <pratitipshah@gmail.com>
2020-01-14 18:01:07 +0530

    Modified with different logic of binary search Head and tail

--- 7_while_loop/2_MagicNo.sh
+++ 7_while_loop/2_MagicNo.sh
@@ -2,38 +2,28 @@
 echo Think a no between 1 to 100
 
 f=0
-np=100
-nc=50
-while [[ $f == 0 ]]
+head=0
+tail=100
+while [[ $head < $tail ]]
 do
-	echo "is no greater than $nc (y/n) : "
-	read lg
-	temp=$nc
-	#newFactor is to be added or subtracted to get new nc depending on l/g value
-	newFactor=$(( (nc-np) / 2 ))
-	if [[ $newFactor -lt 0 ]]
-	then 
-		newFactor=$(( newFactor*(-1) ))
+	if [[ $((tail-head)) != 1 ]]
+	then break
 	fi
+	newFactor=$(( (head+tail) / 2 ))
+	echo "is no greater than $newFactor (y/n) : "
+	read lg
+	#newFactor is to be added or subtracted to get new nc depending on l/g value
+	echo "newFactor=$newFactor"
 	#logic to get new n/2 (nc)
 	if [[ $lg == 'n' ]]
 	then 
-		nc=$(( nc - newFactor )) 
-		np=$temp
+		tail=$newFactor
 	elif [[ $lg == 'y' ]]
 	then
-		nc=$(( nc + newFactor )) 
-		np=$temp
+		head=$newFactor
 	fi
+	echo $head $tail
 
-	#echo "nc = $nc and np = $np"
-	if [[ ($nc -eq $np) || ($newFactor -eq 0) ]]
-	then 
-		if [[ $lg == 'y' ]]
-		then (( np++ ))
-		fi
-		echo "Your no is $np"
-		f=1
-	fi
 done
+echo "Your no is $tail"
 
---
+++ 7_while_loop/backupMagicNo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x
+echo Think a no between 1 to 100
+
+f=0
+np=100
+nc=50
+while [[ $f == 0 ]]
+do
+	echo "is no greater than $nc (y/n) : "
+	read lg
+	temp=$nc
+	#newFactor is to be added or subtracted to get new nc depending on l/g value
+	newFactor=$(( (nc-np) / 2 ))
+	if((newFactor< 0))
+	then 
+		newFactor=$(( newFactor*(-1) ))
+	fi
+	echo "newFactor=$newFactor"
+	#logic to get new n/2 (nc)
+	if [[ $lg == 'n' ]]
+	then 
+		nc=$(( nc - newFactor )) 
+		np=$temp
+	elif [[ $lg == 'y' ]]
+	then
+		nc=$(( nc + newFactor )) 
+		np=$temp
+	fi
+
+	echo "nc = $nc and np = $np"
+	if [[ $nc == $np || $newFactor == 0 ]]
+	then 
+		if [[ $lg == 'y' ]]
+		then ((np++))
+		fi
+			echo "Your no is $np"
+			f=1
+	
+	fi
+done
+echo "Your no is $np"
+
