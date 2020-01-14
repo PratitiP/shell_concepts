aaa236e51cdab0443f71d133e898477ccfd84b07
pratiti <pratitipshah@gmail.com>
2020-01-14 18:34:29 +0530

    Removed: temp file

--- temp.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch -f --env-filter "
-	GIT_AUTHOR_NAME='Pratiti'
-	GIT_AUTHOR_EMAIL='pratitipshah@gmail.com'
-	GIT_COMMITTER_NAME='pratiti'
-	GIT_COMMITTER_EMAIL='pratitipshah@gmail.com'
-" HEAD
